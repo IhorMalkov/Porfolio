07c100b246524d3fe5fcc5770dd357a2bc9aded2
IhorMalkov <igorzno2004@gmail.com>
2024-09-11 14:30:30 -0700

    Fixing header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,69 +2,38 @@ import styles from './Header.module.scss';
 import menuIcon from '../../assets/menu.svg';
 import closeMenuIcon from '../../assets/close.svg';
 import { navLinks } from './HeaderConstants.js';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
     const toggleMenu = () => setIsOpen(!isOpen);
 
-    const handleResize = () => {
-        setIsMobile(window.innerWidth <= 768);
-    };
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
     return (
-        <div>
-            <header className={`${styles.header} ${isOpen ? styles.menuOpen : ''}`}>
-                {isMobile ? (
-                    <>
-                        <a href='/' className={styles.logo}>IhorMalkov</a>
-                        <button onClick={toggleMenu} className={styles.button}>
-                            <img
-                                className={styles.burger}
-                                src={isOpen ? closeMenuIcon : menuIcon}
-                                alt={isOpen ? 'Close menu' : 'Open menu'}
-                            />
-                        </button>
-                        {isOpen && (
-                            <nav className={styles.nav}>
-                                <ul className={styles.navList}>
-                                    {navLinks.map((item) => (
-                                        <li key={item.id} className={styles.navItem}>
-                                            <a href={item.link} className={styles.navLink}>
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </nav>
-                        )}
-                    </>
-                ) : (
-                    <>
-                        <a href='/' className={styles.logo}>IhorMalkov</a>
-                        <nav className={styles.nav}>
-                            <ul className={styles.navList}>
-                                {navLinks.map((item) => (
-                                    <li key={item.id} className={styles.navItem}>
-                                        <a href={item.link} className={styles.navLink}>
-                                            {item.name}
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </nav>
-                    </>
-                )}
-            </header>
-        </div>
+        <header className={isOpen ? styles.header : styles.headerDefault}>
+            <a href='/' className={styles.logo}>IhorMalkov</a>
+
+            <button onClick={toggleMenu} className={styles.button}>
+                <img
+                    className={styles.burger}
+                    src={isOpen ? closeMenuIcon : menuIcon}
+                    alt={isOpen ? 'Close menu' : 'Open menu'}
+                />
+            </button>
+
+            {isOpen &&
+                <nav className={styles.nav}>
+                <ul className={styles.navList}>
+                    {navLinks.map((item) => (
+                        <li key={item.id} className={styles.navItem}>
+                            <a href={item.link} className={styles.navLink}>
+                                {item.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            }
+        </header>
     );
 }
