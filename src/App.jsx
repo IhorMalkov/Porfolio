82f0dff5473f81fe8c6654d778ed72669dfc97d8
Ihor Malkov <igorzno2004@gmail.com>
2024-09-18 23:53:21 -0700

    Starting massive debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './global.css'
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
+import Info from "./components/Info/Info.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className='app-container'>
       <Header/>
         <Hero/>
+        <Info/>
     </div>
   )
 }
--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,13 +2,8 @@ import styles from './Info.module.scss'
 const Info = () => {
     return (
         <section>
-            <div className={styles.grid}>
-                <div className={styles.col}>
-                    <div className={styles.container}>
-                        <img />
-                    </div>
-                </div>
-            </div>
+            <div className={styles.title}><h2 className={styles.text}>My Work</h2></div>
+            <div className={styles.subtitle}><p className={styles.text}>A collection of projects I've worked on.</p></div>
         </section>
     )
 }
