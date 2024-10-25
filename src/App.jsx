df0e4d63b8b6332539febae9d62233915bc58c41
Ihor Malkov <igorzno2004@gmail.com>
2024-10-24 17:20:24 -0700

    Finished ContactMe section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './global.css'
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Info from "./components/Info/Info.jsx";
+import Contact from "./components/contact/Contact.jsx";
 
 function App() {
-
   return (
     <div className='app-container'>
       <Header/>
         <Hero/>
         <Info/>
+      <Contact/>
     </div>
   )
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,35 @@
+import styles from './Contact.module.scss';
+
+export default function Contact() {
+    return (
+        <section className={styles['contact-section']}>
+            <img className={styles['terminal-image']} src='/assets/terminal.png' alt='Terminal Background'/>
+            <div className={styles['form-container']}>
+                <h3>Let's Connect</h3>
+                <h4>
+                    Whether you’re looking to build a new website, improve your<br />
+                    existing platform, or bring a unique project to life, I’m here to<br />
+                    help.
+                </h4>
+                <form>
+                    <label>Full Name</label>
+                    <input type="text" placeholder="ex., Ihor Malkov" />
+                    <label>Email address</label>
+                    <input type="email" placeholder="ex., ihor.malkovv@gmail.com" />
+                    <label>Your message</label>
+                    <textarea
+                        className={styles['text-input']}
+                        name="message"
+                        required
+                        rows={5}
+                        placeholder="Share your thoughts, or inquiries..." />
+                    <button type="submit">
+                        Send Message
+                        <img src="/assets/arrow-up.png" alt="Send" />
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+}
+
